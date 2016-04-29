69e5b95455866af6216e78e47e74ac25402d5234
Boverton <igrproductions@gmail.com>
2016-04-28 20:23:21 -0500

    pulls from DB instead of API
    commented out image until we find one.

--- display.php
+++ display.php
@@ -11,18 +11,21 @@ $sort = 1;
 $search = new SearchResults($location, $category_filter, $sort);
 
 echo '<pre>';
-foreach( $search->results->businesses as $business) {
 
+$results = $search->pull_from_db();
 
-    echo '<h3>' . $business->name . '</h3><br />';
-    echo "<img src=\"$business->image_url\" />" . '<br />';
-    echo "business url <a href=\"$business->url\">website</a>" . '<br />';
-    echo isset($business->display_phone) ? 'Phone number ' . $business->display_phone . '<br />' : 'Phone number is not available <br />';
-    echo 'Rating ' . $business->rating . '<br />';
-    echo 'Reviews ' . $business->review_count . '<br />';
+$image = "http://www.arclightbrewing.com/files/Food%20Truck%20Court.png";
+
+foreach($results as $result) {
+
+    echo '<h3>' . $result['truck_name'] . '</h3><br />';
+    //echo "<img src=" . "'" . $result['m_yelp_url'] . "'" . "/>" . '<br />';
+    echo "business url <a href=\"$image\"></a>" . '<br />';
+    echo !empty($result['phone']) ? 'Phone number ' . $result['phone'] . '<br />' : 'Phone number is not available <br />';
+    echo 'Rating ' . $result['rating'] . '<br />';
+    echo 'Reviews ' . $result['review_count'] . '<br />';
 }
 
-?>
 
 
 <html>
